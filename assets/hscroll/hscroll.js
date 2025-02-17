22635bfbedd8ceda7128a2decb1e9c1f636104d0
Vincent Agnano <vinyll@protonmail.com>
2025-02-18 00:10:02 +0100

    Netflix style

---
+++ assets/hscroll/hscroll.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+  // Scroll functionality for buttons
+  const scrollableSection = document.querySelector(
+    ".scrollable-section"
+  ).firstElementChild
+
+  document.getElementById("scrollLeft").addEventListener("click", () => {
+    scrollableSection.scrollBy({
+      left: -300,
+      behavior: "smooth",
+    })
+  })
+
+  document.getElementById("scrollRight").addEventListener("click", () => {
+    scrollableSection.scrollBy({
+      left: 300,
+      behavior: "smooth",
+    })
+  })
+})
